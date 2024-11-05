67bbc506c73acd8f13fac09e3c4a3c49157f45a8
KaioHenr <120226915+KaioHenr@users.noreply.github.com>
2024-11-05 10:52:35 -0300

    First commit

---
+++ Trab1-PS/Models/filme.cs
@@ -0,0 +1,10 @@
+﻿namespace Trab1_PS.Models;
+
+public class Filme
+{
+    private string Nome { get; set; }
+    private string Descricao { get; set; }
+    private int Avaliacao { get; set; }
+    private string Duracao { get; set; }
+    private Data Datalan { get; set; }
+}
--- Trab1-PS/obj/Debug/net8.0/Trab1-PS.AssemblyInfo.cs
+++ Trab1-PS/obj/Debug/net8.0/Trab1-PS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trab1-PS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4127cb4c0400372272940cad9834918662b5ddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trab1-PS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trab1-PS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
