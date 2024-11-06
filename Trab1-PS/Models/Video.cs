dd0ba451f0d07be80f6b577e3c027536faee7dc3
KaioHenr <120226915+KaioHenr@users.noreply.github.com>
2024-11-06 09:12:35 -0300

    Feature: criação de classes
    Criei classe data e video

---
+++ Trab1-PS/Models/Video.cs
@@ -0,0 +1,22 @@
+﻿namespace Trab1_PS.Models;
+
+public class Video
+{
+    private string nome { set; get; }
+    private List<string> genero { set; get; }
+    private int duracao { set; get; }
+    private string sinopse { set; get; }
+    private Data lancamento { set; get; }
+    private List<string> elenco { set; get; }
+
+    public Video()
+    {
+        //consulta no banco ou busca na api
+    }
+
+    public void avaliacao(int test)
+    {
+        Console.WriteLine(test);
+    }
+    
+}
--- Trab1-PS/obj/Debug/net8.0/Trab1-PS.AssemblyInfo.cs
+++ Trab1-PS/obj/Debug/net8.0/Trab1-PS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trab1-PS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67bbc506c73acd8f13fac09e3c4a3c49157f45a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a4ca8c514d5d701dd2def91a421d12ae82b777")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trab1-PS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trab1-PS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
