a03bbe2124bccf7be4936dfb156b1e42895699c2
Larissa Rangel <113314338+larissabrangel@users.noreply.github.com>
2024-11-13 11:06:01 -0300

    Banco de dados

--- Trab1-PS/AvaliacaoDb.cs
+++ Trab1-PS/AvaliacaoDb.cs
@@ -1,6 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Trab1_PS.Models;
 
 public class AvaliacaoDb : DbContext //Classe avaliacaoDb herda de DbContext para comunicação com o banco de dados
 {
+    public AvaliacaoDb(DbContextOptions<AvaliacaoDb> options)
+        : base(options) { }
+    
+    //Definindo as tabelas no banco
+    public DbSet<Avaliacao> Avaliacoes { get; set; }
+    public DbSet<Filme> Filmes { get; set; }
+    public DbSet<Serie> Series { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    
+    
     
 }
--- Trab1-PS/Program.cs
+++ Trab1-PS/Program.cs
@@ -1,28 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Trab1_PS.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// Adiciona o DbContext e configura o banco de dados (SQLite no exemplo)
+builder.Services.AddDbContext<AvaliacaoDb>(options =>
+    options.UseSqlite("Data Source=avaliacao.db"));
+
+// Adiciona serviços para controllers (somente para API)
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuração do pipeline de requisições para a API
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+// Mapear os controladores da API
+app.MapControllers();
 
 app.Run();
-//teste
