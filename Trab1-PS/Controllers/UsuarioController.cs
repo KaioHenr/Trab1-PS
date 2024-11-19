4d25091543f905b30be114197029146b6c697ac4
Larissa Rangel <113314338+larissabrangel@users.noreply.github.com>
2024-11-18 22:59:53 -0300

    Testando requisições

--- Trab1-PS/Controllers/UsuarioController.cs
+++ Trab1-PS/Controllers/UsuarioController.cs
@@ -1,8 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Trab1_PS.Models;
 
 namespace Trab1_PS.Controllers;
 
 public class UsuarioController : ControllerBase
 {
+    [ApiController]
+    [Route("usuarios")]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateUsuario([FromBody] Usuario usuario)
+        {
+            // Implementação aqui
+            return Ok();
+        }
+    }
+
     
 }
