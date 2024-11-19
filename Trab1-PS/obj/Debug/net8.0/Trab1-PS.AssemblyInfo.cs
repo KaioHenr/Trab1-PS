1199f2b1e6b9838d072181c549ee0afce40a6f31
Larissa Rangel <113314338+larissabrangel@users.noreply.github.com>
2024-11-19 00:21:18 -0300

    Update gitignore

--- Trab1-PS/obj/Debug/net8.0/Trab1-PS.AssemblyInfo.cs
+++ Trab1-PS/obj/Debug/net8.0/Trab1-PS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trab1-PS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48084f36598240ebd2e25c4648811fd5a89ed2df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f346b19aaeb33b8bce387811e35ed5f5cbd3270")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trab1-PS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trab1-PS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
