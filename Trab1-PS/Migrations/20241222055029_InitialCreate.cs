ff931873194733ae5b7994b953a2d8727e768603
Larissa Rangel <113314338+larissabrangel@users.noreply.github.com>
2024-12-22 03:37:59 -0300

    Subindo docker

---
+++ Trab1-PS/Migrations/20241222055029_InitialCreate.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Trab1_PS.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doramas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Titulo = table.Column<string>(type: "text", nullable: false),
+                    Descricao = table.Column<string>(type: "text", nullable: false),
+                    QtdEpisodios = table.Column<int>(type: "integer", nullable: false),
+                    DataLancamento = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doramas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Senha = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Generos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    DoramaId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Generos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Generos_Doramas_DoramaId",
+                        column: x => x.DoramaId,
+                        principalTable: "Doramas",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Avaliacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: false),
+                    DoramaId = table.Column<int>(type: "integer", nullable: false),
+                    Nota = table.Column<int>(type: "integer", nullable: false),
+                    Comentario = table.Column<string>(type: "text", nullable: false),
+                    DataAvaliacao = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Avaliacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Avaliacoes_Doramas_DoramaId",
+                        column: x => x.DoramaId,
+                        principalTable: "Doramas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Avaliacoes_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacoes_DoramaId",
+                table: "Avaliacoes",
+                column: "DoramaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacoes_UsuarioId",
+                table: "Avaliacoes",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Generos_DoramaId",
+                table: "Generos",
+                column: "DoramaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Avaliacoes");
+
+            migrationBuilder.DropTable(
+                name: "Generos");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Doramas");
+        }
+    }
+}
--- Trab1-PS/Program.cs
+++ Trab1-PS/Program.cs
@@ -1,28 +1,17 @@
 using Microsoft.EntityFrameworkCore;
-using Trab1_PS.Models;
-using Trab1_PS.Data; // Adicione este using para o AppDbContext
+using Trab1_PS.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpClient();
 
-// Configurando o Entity Framework Core com InMemory
-// O banco de dados "AppDbContext" será armazenado na memória enquanto a aplicação estiver em execução
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("AppDbContext"));
+// Configurando o Entity Framework Core para usar PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
-// Permite que a aplicação processe requisições HTTP que são para os controladores
 builder.Services.AddControllers();
 
-// Configurando o suporte a sessões de login
-// Usado para armazenar informações temporárias, como login do usuário
-builder.Services.AddDistributedMemoryCache(); // Cache para gerenciar dados de sessão
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(10); // Tempo de expiração da sessão
-});
+var app = builder.Build();
 
-var app = builder.Build(); 
-
-// Mapeia os endpoints configurados nos controladores
 app.MapControllers();
 
 app.Run();
